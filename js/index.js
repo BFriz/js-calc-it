7468fd353ee70b677b9c63ea6c963567341e316f
Ben Frisby <bfrisb@gmail.com>
2015-05-05 20:12:00 +0100

    got everything except for it to equal

--- js/index.js
+++ js/index.js
@@ -1,15 +1,36 @@
 var number = document.getElementsByClassName('number');
 var firstNum ='';
 var firstOp ='';
+var secondNum = '';
+var operator = document.getElementsByClassName('operator');
+var equal = document.getElementsByClassName('eq');
+//find and store the first number input from the user
 for (var i = 0; i< number.length; i++){
   number[i].addEventListener("click", function(e){
-   firstNum = parseInt(this.value);
+   if (firstNum === ''){
+    firstNum = parseInt(this.value);
+  }else{
+    secondNum = parseInt(this.value);
+  }
   });
 }
-
-var operator = document.getElementsByClassName('operator');
+//find and store the operator input from the user
 for (var i = 0; i< operator.length; i++){
   operator[i].addEventListener("click", function(e){
    firstOp = this.value;
   });
 }
+
+  // switch(answer){
+  //   case "-":
+  //   var ans = firstNum - secondNum;
+  //   break;
+  //   case "+":
+  //   var ans = firstNum + secondNum;
+  //   break;
+  //   case "*":
+  //   var ans = firstNum * secondNum;;
+  //   break;
+  //   case "/":
+  //   var ans = firstNum / secondNum;;
+  //   break;
